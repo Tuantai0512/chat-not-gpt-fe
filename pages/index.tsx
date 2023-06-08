9d4f038cc135ad87fc5bc2a683f86f92bcb6fd01
tuyen <tuyen.tran@eastplayers.io>
2023-06-08 17:23:03 +0700

    refactor: login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,90 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { Inter } from 'next/font/google'
-import ChatsNav from "../components/chatsNav"
-import HomeStyle from "../styles/home.module.scss"
-import Modal from '@/components/modal';
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { Inter } from "next/font/google";
+import ChatsNav from "../components/chatsNav";
+import HomeStyle from "../styles/home.module.scss";
+import Modal from "@/components/modal";
 import axios from "axios";
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+// export default function Home() {
+//   const router = useRouter();
+//   let token = '';
+//   let [user, setUser] = useState({});
+//   try {
+//     token = localStorage.getItem("Token") || '';
+//   } catch (error) {
+//     console.log(error)
+//   }
+//   console.log(token);
+//   let verifyToken = async (token: string) => {
+//     let res = await axios.post('http://localhost:8000/api/v1/auth', { token: token.slice(1, token.length - 1) });
+//     let userData = res && res.data ? res.data : [];
+//     setUser(userData);
+//     return userData
+//   };
+//   if (token) {
+//     useEffect(() => {
+//       verifyToken(token);
+//     }, [token])
+//     console.log(user);
+//   }
+//   const isEmptyObj = Object.keys(user).length === 0;
+//   return (
+//     !isEmptyObj &&
+//     <main className={`${HomeStyle['chats-app']} flex`}>
+//       <ChatsNav userInfo={user} />
+//       <div>
+//         <div>Welcome to my chat app</div>
+//         <Modal textBtn="Account settings" />
+//       </div>
+//     </main>
+//   )
+// }
+
+const verifyToken = async (token: string) => {
+  try {
+    const res = await axios.post("http://localhost:8000/api/v1/auth", {
+      token: token.slice(1, token.length - 1),
+    });
+
+    return res?.data;
+  } catch (error) {
+    return undefined;
+  }
+};
 
 export default function Home() {
   const router = useRouter();
-  let token = '';
-  let [user, setUser] = useState({});
-  try {
-    token = localStorage.getItem("Token") || '';
-  } catch (error) {
-    console.log(error)
-  }
-  console.log(token);
-  let verifyToken = async (token: string) => {
-    let res = await axios.post('http://localhost:8000/api/v1/auth', { token: token.slice(1, token.length - 1) });
-    let userData = res && res.data ? res.data : [];
-    setUser(userData);
-    return userData
-  };
-  if (token) {
-    useEffect(() => {
-      verifyToken(token);
-    }, [token])
-    console.log(user);
-  }
-  const isEmptyObj = Object.keys(user).length === 0;
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const token = localStorage.getItem("Token");
+
+    console.log({ token });
+
+    if (token) {
+      verifyToken(token).then((user) => {
+        if (!user) {
+          router.push("/user/login");
+          return;
+        }
+
+        setUser(user);
+      });
+    } else {
+      router.push("/user/login");
+    }
+  }, []);
+
   return (
-    isEmptyObj === false &&
-    <main className={`${HomeStyle['chats-app']} flex`}>
+    <main className={`${HomeStyle["chats-app"]} flex`}>
       <ChatsNav userInfo={user} />
       <div>
         <div>Welcome to my chat app</div>
         <Modal textBtn="Account settings" />
       </div>
     </main>
-  )  
+  );
 }
--- pages/user/login.tsx
+++ pages/user/login.tsx
@@ -1,5 +1,5 @@
-import login from "/styles/login.module.scss"
-import { useState, useEffect } from "react"
+import login from "/styles/login.module.scss";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import axios from "axios";
 import { useRouter } from "next/router";
@@ -7,55 +7,88 @@ import { useDispatch, useSelector } from "react-redux";
 import { userLoginSuccess } from "../../stores/actions/userActions";
 
 export default function Login() {
-
-    const router = useRouter();
-    const dispatch = useDispatch();
-    const userInfoRedux = useSelector((state: any) => state.user);
-    const { userInfo } = userInfoRedux;
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('')
-    const handleSubmit = async (e: any) => {
-        e.preventDefault();
-        const res = await axios.post('http://localhost:8000/api/v1/login', {
-            username, password
-        }, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        }).catch(function (error) {
-            if(error.response){
-                if(error.response.data){
-                    setError(error.response.data.message);
-                }
-            }
-        });
-        let data = res && res.data ? res.data : [];
-        if(data.errCode == 2 || data.errCode == 3){
-            setError(data.message);
-        }else if(data.errCode == 0){
-            dispatch(userLoginSuccess(data));
-            router.push(`/`);
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const userInfoRedux = useSelector((state: any) => state.user);
+  const { userInfo } = userInfoRedux;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const handleSubmit = async (e: any) => {
+    const res = await axios
+      .post(
+        "http://localhost:8000/api/v1/login",
+        {
+          username,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
         }
+      )
+      .catch(function (error) {
+        if (error.response) {
+          if (error.response.data) {
+            setError(error.response.data.message);
+          }
+        }
+      });
+    let data = res && res.data ? res.data : [];
+    if (data.errCode == 2 || data.errCode == 3) {
+      setError(data.message);
+    } else if (data.errCode == 0) {
+      dispatch(userLoginSuccess(data));
+      router.push(`/`);
     }
-    useEffect(() => {
-        if (!userInfoRedux) return;
-        localStorage.setItem('Token', JSON.stringify(userInfoRedux.userInfo.token));
-    }, [userInfoRedux]);
+  };
+  useEffect(() => {
+    if (!userInfoRedux?.userInfo?.token) return;
 
-    return (
-        <div className={login.container}>
-            <div className={login.title}>User Login</div>
-            <form className={login.form} onSubmit={handleSubmit}>
-                <input className={login.input} type="text" placeholder="Username" value={username} name="username" onChange={(e) => { setUsername(e.target.value) }}></input>
-                <input className={login.input} type="password" placeholder="Password" value={password} name="password" onChange={(e) => { setPassword(e.target.value) }}></input>
-                <label style={{ color: "red" }}>{error}</label>
-                <a href="/" className={login.link}>Forgot password?</a>
-                <button type="submit" className={login.button}>Login</button>
-            </form>
-            <div className={login.register}>
-                <p>Don't have an account?<Link href="/user/register" className={login.link}>Sign up</Link></p>
-            </div>
-        </div>
-    )
+    localStorage.setItem("Token", userInfoRedux.userInfo.token);
+  }, [userInfoRedux]);
+
+  return (
+    <div className={login.container}>
+      <div className={login.title}>User Login</div>
+      <form className={login.form} onSubmit={handleSubmit}>
+        <input
+          className={login.input}
+          type="text"
+          placeholder="Username"
+          value={username}
+          name="username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        ></input>
+        <input
+          className={login.input}
+          type="password"
+          placeholder="Password"
+          value={password}
+          name="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
+        <label style={{ color: "red" }}>{error}</label>
+        <a href="/" className={login.link}>
+          Forgot password?
+        </a>
+        <button type="submit" className={login.button}>
+          Login
+        </button>
+      </form>
+      <div className={login.register}>
+        <p>
+          Don't have an account?
+          <Link href="/user/register" className={login.link}>
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
